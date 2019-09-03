bdb9767228ac6bdd78b1e3365da407e75d00ae7b
Marek <info@idiab.dev>
2019-09-03 10:17:38 +0200

    Initial data load to github

---
+++ eDroplet/eDroplet/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace eDroplet.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+    }
+}
